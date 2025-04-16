a2a721d7e731de532002a7f16a72486f03587bcf
Your Name <you@example.com>
2025-04-16 21:24:55 +0100

    add note

--- yapiapp/models.py
+++ yapiapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     content = models.TextField()
 
     def __str__(self):
