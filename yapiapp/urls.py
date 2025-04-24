98a8c16704dd63961e792e44cde20ba50ccb3ecf
Yamina Guenez <yaminaguenez@gmail.com>
2025-04-24 19:21:55 +0100

    full crud operations

--- yapiapp/urls.py
+++ yapiapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('add/', views.add_note, name='add_note'),
+    path('edit/<int:pk>/', views.edit_note, name='edit_note'),
+    path('delete/<int:pk>/', views.delete_note, name='delete_note'),
+
 ]
--- yapiapp/views.py
+++ yapiapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .models import Note
 from .forms import NoteForm
 
@@ -19,3 +20,23 @@ def add_note(request):
          form = NoteForm()
 
     return render(request, 'yapiapp/add_note.html', {'form':form})
+
+
+def edit_note(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance= note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else: 
+        form= NoteForm(instance=note)
+
+    return render(request, 'yapiapp/edit_note.html', {'form':form})
+
+def delete_note(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method== 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'yapiapp/delete_note.html', {'note': note})
